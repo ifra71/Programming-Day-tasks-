8ff3ef13e875b5b9ec32504a95f63c59d31366e8
Abdullah~ <abdullah.fazal@netsoltech.com>
2022-12-20 23:27:40 +0500

    first commit

---
+++ HEHE/hehe.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"              OOOOOOOOOOOOOOOOOOO                      "<<endl;
+cout<<"            ||                  OO                     "<<endl;
+cout<<"            ||   ^^^     ---    OO                     "<<endl;
+cout<<"            OO   (0)     (0)    OO                     "<<endl;
+cout<<"            OO        ?         OO                     "<<endl;
+cout<<"            OO    )-------(     OO                     "<<endl;
+cout<<"            OO      ^^^^^       OO                     "<<endl;
+cout<<"            OO                  OO                     "<<endl;
+cout<<"             OO----------------OO                     "<<endl;
+cout<<"               _____|__O__|_______                       "<<endl;
+cout<<"             ||                 ||                    "<<endl;
+cout<<"             ||*****************||                      "<<endl;
+cout<<"             ||   ITNE ZIADA    ||                     "<<endl;
+cout<<"             ||      TASKS ?    ||                      "<<endl;
+cout<<"             ||*****************||                      "<<endl;
+cout<<"             ||                 ||                      "<<endl;
+cout<<"              -------------------                        "<<endl;
+cout<<"              -------------------                        "<<endl;
+cout<<"                [___]      [___]                     "<<endl;
+}
---
+++ Header of Project/PDtasksheader.txt
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout << "     ****************************************************** " <<endl;
+cout << "     **                                                  ** " <<endl;
+cout << "     **             PROGRAMMING DAY TASKS                ** " <<endl;
+cout << "     **                WEEK 02 HEADER                    ** " <<endl;
+cout << "     **                                                  ** " <<endl;
+cout << "     ****************************************************** " <<endl;
+}
